03a45786013ff883e66ec728eaecea460cbda51d
CHOI JUNHYEONG <choigyuyoung@gmail.com>
2026-08-21 11:42:15 +0900

    refactor: update pyright configuration, remove sync_manager, and add environment template

--- 작업시작_동기화.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001 > nul
-title [K-올라운드] 작업 시작 동기화
-cd /d "%~dp0"
-python "%~dp0sync_manager.py" start
-echo.
-pause
--- 작업종료_동기화.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001 > nul
-title [K-올라운드] 작업 종료/퇴근 동기화
-cd /d "%~dp0"
-python "%~dp0sync_manager.py" finish
-echo.
-pause
